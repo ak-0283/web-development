02beb772b937c4646e10e3180caa0687430cc139
abhaykumar-0285 <abhaykumarghy2005@gmail.com>
2023-11-26 13:58:41 +0530

    javascript strings

---
+++ video 60/index.js
@@ -0,0 +1,35 @@
+console.log("this is string tutorial")
+
+
+let a= "abhay";
+console.log(a)
+console.log(a[0]) //to find the word in first position
+console.log(a[1]) //to find the word in second position
+console.log(a[2]) //to find the word in third position
+console.log(a[3]) //to find the word in forth position
+console.log(a[4]) //to find the word in fifth position
+//console.log(a[5]) undefined
+
+
+
+console.log(a.length) //to find array length
+let real_name="abhay"
+let friend="rohan"
+console.log("his name is " + real_name + " and his friend name is "+ friend)
+console.log(`his name is ${real_name} and his friend name is ${friend}`) //another way to write the statement wihtout using + symbol. and  with backtick symbol
+
+
+
+let b="shivam"
+console.log(b.toUpperCase())                              //word convert to uppercase
+console.log(b.toLowerCase())                              //word convert to lowercase
+console.log(b.length)                                     //find the word in length
+console.log(b.slice(1,4))                                 //to do slice the word in which 1 character is included and 4 not included
+console.log(b.slice(1))                                   //to do show 1 index to last to word
+console.log(b.replace("sh","77"))                         //to replace the new character
+console.log(b.concat(a,"god","rahul","suraj","priya"))    //to concate the words with next word
+
+
+
+
+
