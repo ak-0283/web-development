fceb0824918ea56a34913b9a9b051f2e3dc6a866
abhaykumar-0285 <abhaykumarghy2005@gmail.com>
2023-12-05 13:25:35 +0530

    javascript DOM-childern,parent & sibling nodes

---
+++ video 67/script.js
@@ -0,0 +1,5 @@
+console.log("Hello world")
+
+document.body.firstElementChild //pehla aisa child jo element ho
+document.body.firstElementChild.childNodes //saare nodes aayenge text,comment,text,div.box,etc
+document.body.firstElementChild.children //sirf iske andar ka elements nodes milenge
